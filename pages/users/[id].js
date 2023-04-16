099f24bd7d89c08fbc472bdaea118607de5a827a
DENIS <den465341@gmail.com>
2023-04-16 18:05:17 +0300

    feat: implement main container

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,12 +1,15 @@
 import { useRouter } from 'next/router';
 import s from '../../styles/user.module.scss';
+import MainContainer from '../../components/MainContainer';
 
 export default function User({ user }) {
   const { query } = useRouter();
   return (
     <>
-      <h1>User with id: {query.id}</h1>
-      <div className={s.userName}>{user.name}</div>
+      <MainContainer keywords={user.name}>
+        <h1>User with id: {query.id}</h1>
+        <div className={s.userName}>{user.name}</div>
+      </MainContainer>
     </>
   );
 }
